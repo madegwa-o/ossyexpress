179fdd26f9bc81ce8eb10021fe310bfab9efc8f8
Ubuntu <ubuntu@crashten.com>
2025-12-09 00:54:56 +0000

    base server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express server!');
+});
+
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
