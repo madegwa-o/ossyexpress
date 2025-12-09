b62f6e4061b7eaabc2378659f19a4810e4b452ac
madegwa-o <madegwaoscar317@gmail.com>
2025-12-09 04:03:40 +0300

    server message

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-
+app.use(cors());        // IMPORTANT: allow React to access backend
 app.use(express.json());
-
 
 app.get('/', (req, res) => {
   res.send('Hello from Express server!');
 });
 
+// Example API route
+app.get('/api/data', (req, res) => {
+  res.json({
+    message: "This is data from the Express server",
+    time: new Date()
+  });
+});
 
 const PORT = 4000;
 app.listen(PORT, () => {
